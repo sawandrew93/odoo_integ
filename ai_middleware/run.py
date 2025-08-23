f7adcea66f6687bfdccb3588eade91bd77ac5911
Andrew <sawandrew93@gmail.com>
2025-08-23 16:32:27 +0630

    fix run.py1

--- ai_middleware/run.py
+++ ai_middleware/run.py
@@ -1,5 +1,4 @@
 import uvicorn
-from src.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
